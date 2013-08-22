9290d8c307d90e262b7c1f56639e085d10c0bfe7
Sampat Badhe <sampat.badhe@kiprosh.com>
2013-08-22 21:01:27 +0530

    Display and Search Books

--- books.rb
+++ books.rb
@@ -54,6 +54,14 @@ class Book
 								end	
 
 								$db.query("INSERT INTO books (booksid, name, price, stock, authors) VALUES ('#{@bid}', '#{@bname}', #{@bprice}, #{@bstock}, '#{@bauthors}')")
+<<<<<<< HEAD
+								puts "Book ID      :- #{@bid} "
+								puts "Book Name    :- #{@bname} "
+								puts "Book Price   :- #{@bprice}"
+								puts "Stock        :- #{@bstock}"
+								puts "Book Authors :- #{@bauthors}"	
+=======
+>>>>>>> 8850953d7686e2378c4b140fbecb2d9f41a116ee
 								puts "Record Add Successfully to the database"
 							rescue => e
 								puts "Error occured while inserting a record :- #{e}"
@@ -62,6 +70,73 @@ class Book
 	    	end
   			end
 	end
+<<<<<<< HEAD
+
+	def display_books
+		begin
+			books = $db.query	("SELECT * FROM books")
+			if books.num_rows == 0
+			puts "No result found"
+			return
+			end
+			puts "Displaying all the Records"
+			books.each_hash do |h|
+			puts h.inspect
+			end
+		rescue => e
+			puts "Error :- #{e}"
+		end
+	end
+
+	def find_book
+		begin
+			puts "Enter Book Name or Author of Book to find:"
+			sname = gets.chomp
+			books = $db.query	("SELECT * FROM books WHERE name like '%#{sname}%' OR authors like '%#{sname}%'")
+			if books.num_rows == 0
+				puts "No result found"
+				return
+			end
+			books.each_hash do |h|
+				puts h.inspect
+			end
+		rescue => e
+			puts "Error :- #{e}"
+		end
+	end
+
+	def quit
+		begin
+			$db.close
+			puts "Bye!"
+			exit
+		rescue => e
+			puts "Error :- #{e}"
+		end
+	end
+end
+b = Book.new
+
+loop do
+		puts %q{Please select an option:
+		1. Add the Records in Books table
+		2. Display the Record from Books table
+		3. Find the Book
+		4. Quit }
+
+		case gets.chomp
+		when '1'
+			b.add_book
+		when '2'
+			b.display_books
+		when '3'
+			b.find_book
+		when '4'
+			b.quit
+		end
+	end
+=======
 end
 b = Book.new
 b.add_book
+>>>>>>> 8850953d7686e2378c4b140fbecb2d9f41a116ee
